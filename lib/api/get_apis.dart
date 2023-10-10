641dafe984eab5ee41a1302681b950716d5788b3
Eshetu Lucas <tekalukas@gmail.com>
2023-10-11 02:00:27 +0300

    Completed UI assignment

---
+++ lib/api/get_apis.dart
@@ -0,0 +1,19 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:luvit/api/api_client.dart';
+import 'package:luvit/api/post_apis.dart';
+import 'package:luvit/app/app.logger.dart';
+
+String baseUrl = dotenv.env['BASE_URL']!;
+
+class GetApis {
+  final log = getLogger('PostApis');
+  ApiClient get apiClient => ApiClient();
+  Future<String> changeData(String userId) async {
+    log.i('userId:$userId');
+    final result = await apiClient.get<String>(
+      realTimeDateBase,
+    );
+
+    return result;
+  }
+}
